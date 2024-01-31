ddfedb5f73f35cd81617be88316bc1ce884045f1
Noxyse <mattieu.gedeon@gmail.com>
2024-01-31 08:40:02 +0100

    affichage details pokemon

--- src/PokemonDetail.js
+++ src/PokemonDetail.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from "react-router-dom"
 
 const PokemonDetail = () => {
+    const [pokemonDetails, setPokemonDetails] = useState({});
     let { id } = useParams();
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('https://raw.githubusercontent.com/Purukitto/pokemon-data.json/master/pokedex.json');
+                const jsonData = await response.json();
+                const result = jsonData.filter((pokemon) => pokemon.id == id);
+                setPokemonDetails(result[0]);
+            } catch (error) {
+                console.error('Error fetching Pokemon data:', error);
+            }
+        };
 
+        fetchData();
+    }, []);
     return (
         <div>
             <p>Bonjour</p>
-            {id}
+            {Object.entries(pokemonDetails.name).map((name) => {
+                <p>Coucou</p>
+            })}
         </div>
     );
 };
