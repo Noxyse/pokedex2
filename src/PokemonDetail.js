877348747ee29f81ac8c34c6ed1ecf0db35e3328
Noxyse <mattieu.gedeon@gmail.com>
2024-02-14 08:33:44 +0100

    correction de PokemonDetail.js et ajout de css

--- src/PokemonDetail.js
+++ src/PokemonDetail.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
+import "./PokemonDetail.css"
 
 const PokemonDetail = () => {
     const [pokemonDetails, setPokemonDetails] = useState({});
@@ -9,7 +10,7 @@ const PokemonDetail = () => {
             try {
                 const response = await fetch('https://raw.githubusercontent.com/Purukitto/pokemon-data.json/master/pokedex.json');
                 const jsonData = await response.json();
-                const result = jsonData.filter((pokemon) => pokemon.id == id);
+                const result = jsonData.filter((pokemon) => pokemon.id === parseInt(id));
                 setPokemonDetails(result[0]);
             } catch (error) {
                 console.error('Error fetching Pokemon data:', error);
@@ -17,13 +18,22 @@ const PokemonDetail = () => {
         };
 
         fetchData();
-    }, []);
+    }, [id]);
     return (
         <div>
             <p>Bonjour</p>
-            {Object.entries(pokemonDetails.name).map((name) => {
-                <p>Coucou</p>
-            })}
+            {pokemonDetails.id && ( /* lors du premier rendu du component, il n'a pas le temps d'aller chercher les données avec le fetch
+                                        donc on va faire un affichage conditionnel avec && (https://react.dev/learn/conditional-rendering#logical-and-operator-)
+                                        Pour faire simple : si pokemonDetails.id existe, alors affiche ce que je te donne entre parenthèse */
+                <div>
+                    <h1>{pokemonDetails.id}</h1>
+                    <img src={pokemonDetails.image.hires} alt={pokemonDetails.name.french} />
+                    <p>Nom : {pokemonDetails.name.french}</p>
+                    <ul>Type :
+                        {pokemonDetails.type.map((type, index) => <li key={index} >{type}</li>)} {/* les types sont un array, donc on map dessus sans oublier la key */}
+                    </ul>
+                </div>
+            )}
         </div>
     );
 };
